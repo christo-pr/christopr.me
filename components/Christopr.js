5ab35ff779d71338cb5857d90b71e81bb392966e
Christofer Flores <cristofer.flort@gmail.com>
2020-11-10 16:39:20 -0600

    Add basic color palette and layout

---
+++ components/Christopr.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+import styles from "../styles/Christopr.module.css"
+
+export default function Christopr(props) {
+  return (
+    <div className={styles["christopr-grid"]}>
+      <div>item</div>
+      <div>item</div>
+      <div>item</div>
+      <div>item</div>
+      <div>item</div>
+      <div>item</div>
+      <div>item</div>
+      <div>item</div>
+      <div>item</div>
+      <div>item</div>
+      <div>item</div>
+      <div>item</div>
+    </div>
+  )
+}
---
+++ components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+import styles from "../styles/Footer.module.css"
+
+export default function Footer(props) {
+  return (
+    <footer className={styles.footer}>
+      Made with ❤️ - &nbsp;
+      <a
+        href="https://github.com/christo-pr"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        christo-pr
+      </a>
+    </footer>
+  )
+}
