0d42359944cdd1ed382f8171e8518137170180b7
Christofer Flores <cristofer.flort@gmail.com>
2020-11-30 17:49:30 -0600

    Update favicon

--- pages/_app.js
+++ pages/_app.js
@@ -5,12 +5,28 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Turret+Road&display=swap"
           rel="stylesheet"
         />
+        <meta
+          name="description"
+          content="Welcome to my personal website! Hope you like it!"
+        ></meta>
+        <meta name="keywords" content="Javascript, React, Programming"></meta>
+        <meta name="author" content="Christofer Flores"></meta>
+        <meta property="og:title" content="Christofer Flores Website"></meta>
+        <meta
+          property="og:description"
+          content="Welcome to my personal website! Hope you like it!"
+        ></meta>
+        <meta
+          property="og:image"
+          content="https://avatars3.githubusercontent.com/u/14341735?s=60&v=4"
+        ></meta>
+        <meta property="og:url" content="https://christopr.me"></meta>
       </Head>
       <Component {...pageProps} />
     </>
